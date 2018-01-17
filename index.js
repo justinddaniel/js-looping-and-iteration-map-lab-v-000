4337a3cfd6945f1bf8d39fbbb3aaf2a37f8a8cf3
Justin Daniel <justinddaniel@gmail.com>
2018-01-17 04:11:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function lowerCaseDrivers (array) {
   let newarray = array.map(function (name) { return name.toLowerCase();});
   return newarray;
 }
+
+function nameToAttributes (array) {
+  let newarray = array.map(function (name) { let namearray = name.split(" "); 
+    return {'firstName': namearray[0], 'lastName': namearray[1]};  });
+}
